5929ef9366782859c923dacf49dc3f1249d196de
harada_k <t2s.single@gmail.com>
2020-08-30 19:40:08 +0900

    first commit

---
+++ application/controllers/time_message.php
@@ -0,0 +1,27 @@
+<?php
+    class Time_message extends CI_Controller {
+
+        function index() {
+            // タイトル
+            $data['title'] = '時刻メッセージ';
+
+            // 現在の時刻
+            $data['now_time'] = date("H時i分s秒");
+
+            // 現在の時刻（時）
+            $now_hour = date("H");
+
+            // 時間に合わせたメッセージを表示
+            if ($now_hour == 12) {
+                $data['message'] = 'お昼です';
+            }elseif ($now_hour == 3) {
+                $data['message'] = '3時のピーナッツのおやつです。';
+            }else{
+                $data['message'] = '今日も頑張って！';
+            }
+
+            // テンプレートに変数を代入
+            $this->load->view('time_message_view', $data);
+        }
+    }
+?>
---
+++ application/views/time_message_view.php
@@ -0,0 +1,17 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>最初のぺーこ</title>
+
+</head>
+
+<body>
+    <?php
+    $now_time
+    ?>
+<br />
+<?php $message ?>
+</body>
+
+</html>
